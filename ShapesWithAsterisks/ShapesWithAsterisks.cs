ada55e1ab702a072f8854c889662100179ae5d79
Andrew Connelly <andrew.connelly@spgusa.com>
2018-06-27 14:16:31 -0400

    Add project files.

---
+++ ShapesWithAsterisks/ShapesWithAsterisks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesWithAsterisks
+{
+    class ShapesWithAsterisks
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********     ***       *         *     ");
+            Console.WriteLine("*       *   *     *    ***       * *    ");
+            Console.WriteLine("*       *  *       *  *****     *   *   ");
+            Console.WriteLine("*       *  *       *    *      *     *  ");
+            Console.WriteLine("*       *  *       *    *     *       * ");
+            Console.WriteLine("*       *  *       *    *      *     *  ");
+            Console.WriteLine("*       *  *       *    *       *   *   ");
+            Console.WriteLine("*       *   *     *     *        * *    ");
+            Console.WriteLine("*********     ***       *         *     ");
+            Console.ReadKey();
+        }
+    }
+}
